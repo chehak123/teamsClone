a10682c450b591de160ab88d08a2a033f111a540
chehak123 <chehakagrawal01@gmail.com>
2021-06-28 11:46:10 +0530

    updated

--- app.js
+++ app.js
@@ -133,4 +133,4 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000
 
-server.listen(PORT, () => console.log(`Listening on port ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
